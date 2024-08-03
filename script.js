6b5957caaa4ef032f9a41b159cc43eb3ed666463
pardeep <kumarpardeep87010@gmail.com>
2024-08-03 21:52:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let noClickCount = 0;
 let isYesClicked = false;
 const bgMusic = document.getElementById('bgMusic');
-const yesMusic = document.getElementById('yesMusic');   
+const yesMusic = document.getElementById('yesMusic'); 
+
 
 const images = [
     "no2.gif", // First image
@@ -17,9 +18,17 @@ const texts = [
     "I promise I'll make it right!",
     "Can we please talk and resolve this?"
 ];
-bgMusic.play();
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("bgMusic");
+    audio.play().catch(function(error) {
+        console.log("Autoplay was prevented:", error);
+    });
+});
+
 
 window.onload = () => {
+    
     
     document.querySelector('.container').style.opacity = 1;
     document.querySelector('.container').style.filter = 'blur(40)';
@@ -28,11 +37,14 @@ window.onload = () => {
         document.querySelector('.buttons').style.filter = 'blur(40)';
     }, 2000); // Delay for showing buttons
 
-    // Start background music on page load
+     // Start background music on page load
+     bgMusic.play();
 };
 
+
+
 function noClicked() {
-    // bgMusic.play(); 
+    bgMusic.play(); 
     
     if (noClickCount < 4) {
         document.getElementById('mainImage').src = images[noClickCount];
@@ -71,7 +83,7 @@ function yesClicked() {
         Miss me<br>
         <button style="border: none;">
             <a href="https://wa.me/9466503764" class="whatsapp-link" style="text-decoration: none; color: white;">
-                Here I am
+                here I am
             </a>
         </button>
     `;
